cfc47263dbca8d47c5edb0d1339ca2262ea48c24
AnushkaRajKashyap <anushkarajkashyap1@gmail.com>
2023-01-13 12:35:06 +0530

    initial commit

---
+++ __tests__/test.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+describe("First Test",()=>{
+    test("First Test",()=>{
+        expect(1).toBe(1);
+    })
+})
---
+++ app.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (request, response) => {
+    response.send("hello world");
+  });
+
+  module.exports = app;
